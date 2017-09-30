24f7a638a2791545444ea5bfb799be153b96a9ab
Юрий Сорокин <u40@Urijs-MacBook-Pro.local>
2017-09-30 05:04:33 +0300

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Юрий Сорокин on 30/09/2017.
+//  Copyright © 2017 ITMO University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
