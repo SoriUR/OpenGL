893848cfc3fb765d01a64ab0af140d11e0208a76
Юрий Сорокин <u40@Urijs-MacBook-Pro.local>
2017-10-07 20:18:33 +0300

    Initial Commit

---
+++ Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game
+//
+//  Created by Юрий Сорокин on 07/10/2017.
+//  Copyright © 2017 ITMO University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
