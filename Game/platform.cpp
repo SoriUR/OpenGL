845805ece626e9f54fc9426783346554e844a913
Юрий Сорокин <u40@Urijs-MacBook-Pro.local>
2017-10-13 03:09:07 +0300

    changed moving alg

---
+++ Game/platform.cpp
@@ -0,0 +1,9 @@
+//
+//  platform.cpp
+//  Game
+//
+//  Created by Юрий Сорокин on 13/10/2017.
+//  Copyright © 2017 ITMO University. All rights reserved.
+//
+
+#include "platform.hpp"
---
+++ Game/platform.hpp
@@ -0,0 +1,14 @@
+//
+//  platform.hpp
+//  Game
+//
+//  Created by Юрий Сорокин on 13/10/2017.
+//  Copyright © 2017 ITMO University. All rights reserved.
+//
+
+#ifndef platform_hpp
+#define platform_hpp
+
+#include <stdio.h>
+
+#endif /* platform_hpp */
